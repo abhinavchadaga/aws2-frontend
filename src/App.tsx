15de120675b0a0ffa49390b6f73af6a8929929a0
abhinavchadaga <abhinav.chadaga@utexas.edu>
2024-01-01 12:35:19 +0530

    init setup and login page

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { AuthenticationTitle } from './AuthenticationTitle.tsx';
+
+const App: React.FC = () => {
+  return <AuthenticationTitle />;
+};
+
+export default App;
